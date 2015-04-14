675d05a23113f2f4f065bd9b36ad7c87e890815a
Marc Nischan <marcnischan@gmail.com>
2015-04-14 14:22:15 -0400

    Adding the json file and setting up the controller to get it

---
+++ app/dictator.factory.js
@@ -0,0 +1,19 @@
+(function() {
+
+    angular.module('dictator').factory('DictatorFactory', DictatorFactory);
+
+    function DictatorFactory($http) {
+        var url = './json/demo-data.json';
+        var factory = {};
+        factory.getDictator = function() {
+            return $http.get(url);
+        };
+        factory.addDictator = function(dictator) {
+            return $http.post(url, dictator);
+        };
+        factory.curIndex = 0;
+
+        return factory;
+    }
+
+})();
--- app/layout/layout.controller.js
+++ app/layout/layout.controller.js
@@ -3,7 +3,20 @@
 		.module('dictator')
 		.controller('LayoutController', Controller);
 
-		function Controller($location){
+		function Controller($location, DictatorFactory){
 			var vm = this;
+			vm.dictators = [];
+			getDictator();
+
+			function getDictator(){
+				// console.log('this is getDictator');
+				DictatorFactory.getDictator()
+					.success(function(data){
+						vm.dictators = data;
+						console.log("You've reached vm.dictators", vm.dictators);
+
+					});
+			}
 		}
+
 })();
