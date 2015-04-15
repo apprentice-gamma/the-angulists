a23d099ad347f9ab851a0992f07cd7162a77badf
Linda Swanson <linda.swanson@detroitlabs.com>
2015-04-15 09:22:44 -0400

    Creating DictatorController, fixed some syntax

--- app/layout/layout.controller.js
+++ app/layout/layout.controller.js
@@ -3,20 +3,14 @@
 		.module('dictator')
 		.controller('LayoutController', Controller);
 
-		function Controller($location, DictatorFactory){
-			var vm = this;
-			vm.dictators = [];
-			getDictator();
+    function Controller($location, DictatorFactory) {
+        var vm = this;
+        vm.title = 'Layout Controller';
+        vm.returnToHome = returnToHome;
 
-			function getDictator(){
-				// console.log('this is getDictator');
-				DictatorFactory.getDictator()
-					.success(function(data){
-						vm.dictators = data;
-						console.log("You've reached vm.dictators", vm.dictators);
-
-					});
-			}
-		}
+        function returnToHome() {
+            $location.url('/');
+        }
+    }
 
 })();
